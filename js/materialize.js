34066e5616c0bed3ce181101f75ae6d91ea0fc77
Jcarlos-rl <carlos_-619@hotmail.com>
2019-07-12 12:01:09 -0500

    Preview servicios

--- js/materialize.js
+++ js/materialize.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
     });
     $('.sidenav').sidenav();
     $('.materialboxed').materialbox();
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {
+        'onOpenEnd': initCarouselModal
+    });
+
+    function initCarouselModal() {
+        var elems = document.querySelectorAll('.carousel');
+        var instances = M.Carousel.init(elems, {'fullWidth': true, 'indicators':true});
+    }
 });
